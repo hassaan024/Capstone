4a9095f36d57b421121711fdbbb026c679dc91a8
haydensgithub <haydenbrimage@gmail.com>
2026-01-02 22:22:14 -0600

    UE project push

---
+++ LeafyLedger/Source/LeafyLedger/LeafyLedger.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LeafyLedger.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LeafyLedger, "LeafyLedger" );
